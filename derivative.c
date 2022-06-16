a8c93f9cea5682ef390628d7b9a8bc9d59ff6f95
Mukund Ramakrishnan <mramakri@outlook.com>
2022-06-16 17:07:21 -0400

    This is the derivative function

---
+++ derivative.c
@@ -0,0 +1,31 @@
+
+Polynomial_t differentiatePoly (Polynomial_t myPoly)
+
+{
+
+// This is a root-finding program that will take the derivative of a polynomial.
+// It depends on what the structure is
+
+
+Polynomial_t differentiatedPoly;
+
+
+
+
+    double* differentiatedPoly.coefficients = (double*)malloc(sizeof(double) * (myPoly.degree + 1 - 1));
+
+
+differentiatedPoly.degree = myPoly.degree - 1;
+
+for (int counter = myPoly.degree; counter > 0; counter--){
+
+differentiatedPoly.coefficients[counter-1] = myPoly.coefficients[counter] * (counter);
+
+}
+
+// That loop should give out the differentiated polynomial. 
+
+
+
+
+}
---
+++ derivative.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+// The differentiatePoly function takes in a Polynomial_t object as input,
+// differentiates it, and returns a differentiated Polynomial_t object 
+// as output. 
+
+Polynomial_t differentiatePoly(Polynomial_t myPoly);
+
