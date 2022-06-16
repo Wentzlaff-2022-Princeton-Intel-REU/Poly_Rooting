1e78e330e0aa92f3bfd5bcef4376a1514218701a
Dara Oseyemi <doseyemi@princeton.edu>
2022-06-16 17:56:55 -0400

    formatting

--- derivative.h
+++ derivative.h
@@ -6,6 +6,8 @@
 
 #include "polynomial.h"
 
+/*--------------------------------------------------------------------*/
+
 // The differentiatePoly function takes in a Polynomial_t object as input,
 // differentiates it, and returns a differentiated Polynomial_t object 
 // as output. 
