a3eef0555771574d69d377b0f5ae770509b411be
Manya Zhu <manya.zhu@gmail.com>
2022-06-16 17:55:14 -0400

    reformatting derivative.c

--- derivative.c
+++ derivative.c
@@ -3,35 +3,25 @@
 /* Author: Mukund Ramakrishnan                                        */
 /*--------------------------------------------------------------------*/
 
+#include <stdlib.h>
 #include "derivative.h"
 
 /*--------------------------------------------------------------------*/
 
-
-
-// This is a root-finding program that will take the derivative of a polynomial.
-// It depends on what the structure is
-
-
+/* This is a root-finding program that will take the derivative of a polynomial. 
+It depends on what the structure is. */
 
 Polynomial_t differentiatePoly(Polynomial_t myPoly) {
 
+    Polynomial_t differentiatedPoly;
 
+    differentiatedPoly.coefficients = (double*)malloc(sizeof(double) * (myPoly.degree + 1 - 1));   
+    differentiatedPoly.degree = myPoly.degree - 1;
 
+    for (int counter = myPoly.degree; counter > 0; counter--){
+        differentiatedPoly.coefficients[counter-1] = myPoly.coefficients[counter] * (counter);
+    }
+    // That loop should give out the differentiated polynomial. 
 
-    double* differentiatedPoly.coefficients = (double*)malloc(sizeof(double) * (myPoly.degree + 1 - 1));
-
-
-differentiatedPoly.degree = myPoly.degree - 1;
-
-for (int counter = myPoly.degree; counter > 0; counter--){
-
-differentiatedPoly.coefficients[counter-1] = myPoly.coefficients[counter] * (counter);
-
+    return differentiatedPoly;
 }
-
-// That loop should give out the differentiated polynomial. 
-
-
-
-
