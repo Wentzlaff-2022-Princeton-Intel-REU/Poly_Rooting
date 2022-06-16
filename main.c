47876ce5f6a548ce983ede7f09b3e102bbe751a1
Manya Zhu <manya.zhu@gmail.com>
2022-06-16 17:31:22 -0400

    Main method fixed semicolon

--- main.c
+++ main.c
@@ -4,18 +4,24 @@
 /*------------------------------------------------------------*/
 
 #include <stdlib.h>
+#include "reading.h"
+#include "newton.h"
+#include "derivative.h"
 
 /*------------------------------------------------------------*/
 
 int main(int argc, char *argv[]) {
 
     if(argc != 2){
-        fprintf("stderr, "Usage: %s critical conversion\n", argv[0]")
+        fprintf(stderr, "Usage: %s critical conversion\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    double value;
     Polynomial_t poly = readPoly();
-    int crit_conversion = argv[1];
-    
-    
+    Polynomial_t polyd = diffrentiatePoly(poly);
+    double crit_conversion = strtod(argv[1], NULL);
+    value = guess(poly, polyd, crit_conversion);
+    printf("The root approximation is: %lf \n", value);
 
+    return 0;
 }
