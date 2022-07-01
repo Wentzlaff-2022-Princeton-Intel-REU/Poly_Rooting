5afa218e3231a1c3eade8628b85c34ee913c839f
m-ukund <mramakri@outlook.com>
2022-07-01 18:28:48 -0400

    Update vecHorner.c

--- vecHorner.c
+++ vecHorner.c
@@ -1,62 +1,44 @@
 /*--------------------------------------------------------------------*/
-/* vecHorner.c  [incomplete]                                          */
+/* vecHorner.c                                                        */
 /*--------------------------------------------------------------------*/
 
 #include "horner.h"
 #include <riscv_vector.h>
 
 
-
 /*--------------------------------------------------------------------*/
 
 double* vecEvaluate(Polynomial_t poly, double* x) {
   
-    
+    solution = (double*) malloc (sizeof(double) * x.size()/sizeof(double)); 
 
-    double32_t* addend1 = poly.coefficients;
-
-    double32_t* solution;
-
-
+    // declare vector registers
     vfloat64m1_t va, vb, vc;
-
-
 
     size_t avl = poly.degree;
 
     for (size_t vl; (vl = vsetvl_e32m1(avl)) > 0; avl -= vl) {
 
-       // put a vector here 
-        //  every single vector element is the coefficient of the highest degree (insert a vector here)
+
+        vc = vfmv_v_f_f64m(poly.coefficients[poly.degree]);
+
+        // This is the vector with our guesses (x vector).
+        vb = vle64_v_f64m1(x, vl); 
 
         for (int i = poly.degree; i > 0; i--){
 
-            va = vle32_v_f64m1(addend1, vl);
-            vc = vadd_vv_f64m1(va, vb, vl);
+            // We are moving one of the polynomial's coefficients (starting with the degree largest one - 1 and moving to the degree lowest) to a vector.
+            va = fmv_v_f_f64m(poly.coefficients[i-1], vl); 
 
+            // We are multiply-adding this along with the x vector (our guesses).
+            vc = vfmadd_vv_f64m1(vc, vb, va, vl); 
+            
         }
 
-       
+        // We are storing our results in the solution array.
+       vse64_v_f64m1(&solution[i - avl], vc, vl);  
 
-
-
-        vc = vadd_vv_i32m1(va, vb, vl);
-        vse32_v_i32m1(sum, vc, vl);
-        addend1 += vl;
-        addend2 += vl;
-        sum += vl;
-    }
-
-
-
-    
- // comment this snippet out when done (below)
-
-    for(int i = poly.degree; i > 0; i--) {
-        solution = poly.coefficients[i - 1] + (solution * x);
-    }
-
-    // 
+    } 
 
     return solution;
 }
