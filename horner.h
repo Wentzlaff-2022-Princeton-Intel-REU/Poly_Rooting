2dc40a1a68a4753c3e868f2590d75e003d506979
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-13 14:37:24 -0400

    formating

--- horner.h
+++ horner.h
@@ -7,7 +7,7 @@
 
 /*------------------------------------------------------------*/
 
-/* The evaluate function takes in a polynomial and an input (double), 
-then evaluates the polynomial with the given input and returns
-the correct value using the Horner method. */
+/* The evaluate function takes in a polynomial and an input
+(double), then evaluates the polynomial with the given input and
+returns the correct value using the Horner method. */
 double evaluate(Polynomial_t poly, double x);
